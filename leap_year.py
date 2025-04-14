b0c30b217de3a882a0ae774424278f35d644f4e7
trinimaydana <tmaydana@mail.austral.edu.ar>
2025-04-14 12:41:30 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,13 @@
 def leap_year():
-    print("TO DO")
+
+    año = int(input("Ingrese un año:"))
+
+    if(año % 4 == 0) and (año % 100 != 0): 
+        respuesta = "es bisiesto"
+    elif(año % 4 == 0) and (año % 400 == 0):
+        respuesta = "es bisiesto"
+    else:
+        respuesta = "no es bisiesto"
+
+    print (f"El año {año} {respuesta}")
+
